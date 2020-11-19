ffcd1925154b96aec67c1ae26eb24758380c61a1
Renault Scabora <renaultscabora@hotmail.com>
2020-11-19 11:20:14 -0300

    implement mult-params to ls

--- ls/ls.c
+++ ls/ls.c
@@ -1,22 +1,42 @@
 #include <dirent.h>
 #include <stdio.h>
 
-int main(int argc, char **argv) {
-  
-  char *path = "./";
-
-  if (argv[1] != NULL) {
-    path = argv[1];
-  }
-
+void listPath(char *path) {
+   
   DIR *full_directory = opendir(path);
   struct dirent *directory_reader;
   
   if (full_directory) {
     
+    printf("%s:\n", path);
+
     while (directory_reader = readdir(full_directory)) {
-      printf("%s\n", directory_reader->d_name);
+      printf("  %s\n", directory_reader->d_name);
     }
+      
+  }
+
+}
+
+int main(int argc, char **argv) {
+  
+  if (argc == 1) {
+    listPath("./");
+    return 0;
+  }
+
+  listPath(argv[1]);
+
+  for (int i=2; i<argc; i++) {
+    
+    char *path = "./";
+
+    if (argv[i] != NULL) {
+      path = argv[i];
+    }
+    
+    puts("\n");
+    listPath(path);
 
   }
   
