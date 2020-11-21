59e9df453196d7322bbfafc5c198f279ee4b8b30
Renault Scabora <renaultscabora@hotmail.com>
2020-11-21 05:32:22 -0300

    update compile-all script

--- compile-all.sh
+++ compile-all.sh
@@ -3,3 +3,5 @@ gcc ./ls/ls.c -o ./compiled/ls
 gcc ./mkdir/mkdir.c -o ./compiled/mkdir
 gcc ./rm/rm.c -o ./compiled/rm
 gcc ./rmdir/rmdir.c -o ./compiled/rmdir
+gcc ./rename/rename.c -o ./compiled/rename
+gcc ./cp/cp.c -o ./compiled/cp
